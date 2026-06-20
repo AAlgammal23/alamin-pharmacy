d224f4a32f6e12e0ad80864683f9e1ee5f8846e9
AAlgammal23 <143761202+AAlgammal23@users.noreply.github.com>
2026-06-20 18:33:38 +0300

    Update admin.js

--- admin-web/js/admin.js
+++ admin-web/js/admin.js
@@ -1,31 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ============ FIREBASE CONFIG ============
+// قيم مشروعك من Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyAhl8TvXaE2sm8VkH2TTljVdsqmdcC2Afs",
   authDomain: "alamin-pharma.firebaseapp.com",
   projectId: "alamin-pharma",
   storageBucket: "alamin-pharma.firebasestorage.app",
   messagingSenderId: "773527249793",
-  appId: "1:773527249793:web:87e12e986e7cf72cb73e80",
-  measurementId: "G-ZSQ0HQPLJ3"
+  appId: "1:773527249793:web:87e12e986e7cf72cb73e80"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import {
   getFirestore, collection, doc, getDocs, getDoc, setDoc, addDoc, deleteDoc,
-  onSnapshot, query, orderBy, where, updateDoc, serverTimestamp
+  onSnapshot, query, orderBy, updateDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const app = initializeApp(firebaseConfig);
